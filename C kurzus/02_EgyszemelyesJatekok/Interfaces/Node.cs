9b85139e6a97dafa177906e7f17f977bcd8306db
gyorkistamas <tamasgyorkis@gmail.com>
2024-03-12 15:07:41 +0100

    mélységi

---
+++ C kurzus/02_EgyszemelyesJatekok/Interfaces/Node.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_EgyszemelyesJatekok.Interfaces
+{
+    public class Node
+    {
+        public State State {  get; set; }
+
+        public int Depth { get; set; }
+
+        public Node Parent { get; set; }
+
+        public int OperatorIndex { get; set; }
+
+        public Node(State state, Node parent = null)
+        {
+            Parent = parent;
+            State = state;
+            OperatorIndex = 0;
+
+            if (parent == null)
+                Depth = 0;
+            else
+                Depth = parent.Depth + 1;
+        }
+
+        public override bool Equals(object? obj)
+        {
+           if (obj == null || !(obj is Node)) return false;
+
+           Node node = obj as Node;
+
+           return State.Equals(node.State);
+        }
+
+        public bool IsTargetNode()
+        {
+            return State.IsTargetState();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sr = new StringBuilder();
+
+            if (Parent != null)
+            {
+                sr.AppendLine(Parent.ToString());
+                sr.AppendLine("----------------------");
+            }
+
+            sr.AppendLine($"Depth: {Depth}");
+            sr.AppendLine(State.ToString());
+
+            return sr.ToString();
+        }
+    }
+}
---
+++ C kurzus/02_EgyszemelyesJatekok/Solvers/DepthFirst.cs
@@ -0,0 +1,87 @@
+﻿using _02_EgyszemelyesJatekok.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_EgyszemelyesJatekok.Solvers
+{
+    public class DepthFirst : Solver
+    {
+        // HF: szélességi keresés megírása (BreadthFirst)
+        // 1. Ezt kell megváltoztatni
+        public Stack<Node> OpenNodes { get; set; } = new Stack<Node>();
+        public List<Node> ClosedNodes { get; set; } = new List<Node>();
+
+        public Node CurrentNode { get; set; }
+
+        public Node Path {  get; set; }
+
+        public DepthFirst(OperatorGenerator operatorGenerator, State startingState)
+            : base (operatorGenerator)
+        {
+            OpenNodes.Push(new Node(startingState));
+        }
+
+        public Operator SelectOperator()
+        {
+            int index = CurrentNode.OperatorIndex++;
+            while(index < OperatorGenerator.Operators.Count)
+            {
+                if (OperatorGenerator.Operators[index].IsApplicable(CurrentNode.State))
+                {
+                    return OperatorGenerator.Operators[index];
+                }
+                index = CurrentNode.OperatorIndex++;
+            }
+
+            return null;
+        }
+
+        public override void Solve()
+        {
+            Path = null;
+
+            while(OpenNodes.Count > 0)
+            {
+                // Kivesszük a legmélyebbi csomópontot
+                CurrentNode = OpenNodes.Pop();
+                // Mivel kiválasztottuk kiterjesztésre, ezért átrakjuk a zártak közé.
+                ClosedNodes.Add(CurrentNode);
+
+                // Ide az elágazás
+
+                // Kiterjesztés
+                Operator selectedOperator = SelectOperator();
+                while(selectedOperator != null)
+                {
+                    State newState = selectedOperator.Apply(CurrentNode.State);
+                    Node newNode = new Node(newState, CurrentNode);
+                    // 2. Szélességi esetén: Belsp ciklus elé kerül és nem a newNode-ot vizsgáljuk, hanem CurrentNode-t
+                    if (newNode.IsTargetNode())
+                    {
+                        Path = newNode;
+                        break;
+                    }
+                    if (!OpenNodes.Contains(newNode) && !ClosedNodes.Contains(newNode))
+                    {
+                        OpenNodes.Push(newNode);
+                    }
+                    selectedOperator = SelectOperator();
+                }
+            }
+
+            if (Path != null)
+            {
+                Console.WriteLine("Solution found: ");
+                Console.WriteLine("----------------");
+                Console.WriteLine(Path);
+            }
+            else
+            {
+                Console.WriteLine("Can't solve!");
+            }
+        }
+    }
+}
